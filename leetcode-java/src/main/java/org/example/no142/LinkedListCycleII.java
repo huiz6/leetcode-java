9e93de80b337ef1586abec5feae473e28cdda032
huiz <hzease@gmail.com>
2024-02-06 19:42:46 -0800

    [Add] no142 linked-list-cycle-ii solution code

---
+++ leetcode-java/src/main/java/org/example/no142/LinkedListCycleII.java
@@ -0,0 +1,45 @@
+package org.example.no142;
+
+import java.util.Objects;
+
+public class LinkedListCycleII {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (true) {
+            if (Objects.isNull(fast) || Objects.isNull(fast.next)) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        fast = head;
+        while(true) {
+            if (fast == slow) {
+                return fast;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node2;
+        System.out.println(new LinkedListCycleII().detectCycle(node1));
+    }
+
+}
---
+++ leetcode-java/src/main/java/org/example/no142/ListNode.java
@@ -0,0 +1,16 @@
+package org.example.no142;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(val);
+    }
+}
