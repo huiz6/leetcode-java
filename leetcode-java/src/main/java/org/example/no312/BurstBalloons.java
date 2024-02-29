da40cf1c6a689d0d5fe633d9220b401ceff648dd
huiz <hzease@gmail.com>
2024-02-28 17:56:41 -0800

    [Add] no312 burst-balloons code (unsolved)

---
+++ leetcode-java/src/main/java/org/example/no312/BurstBalloons.java
@@ -0,0 +1,13 @@
+package org.example.no312;
+
+public class BurstBalloons {
+
+    public int maxCoins(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
