11b97521ef6aba651828f32f18ccec279c2552d0
huiz <hzease@gmail.com>
2024-03-04 23:21:58 -0800

    [Add] no136 single-number solution code

---
+++ leetcode-java/src/main/java/org/example/no136/SingleNumber.java
@@ -0,0 +1,17 @@
+package org.example.no136;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
