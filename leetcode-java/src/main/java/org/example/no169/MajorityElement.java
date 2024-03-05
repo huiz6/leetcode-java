453aacd45ee2a5e7616f1067fa558950c52fb22d
huiz <hzease@gmail.com>
2024-03-04 19:19:58 -0800

    [Add] no169 majority-element solution code

---
+++ leetcode-java/src/main/java/org/example/no169/MajorityElement.java
@@ -0,0 +1,24 @@
+package org.example.no169;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0], count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
