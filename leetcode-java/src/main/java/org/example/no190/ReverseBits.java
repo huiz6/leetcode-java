1de029619b6728687f8a1629b34e6712b7580ce7
huiz <hzease@gmail.com>
2024-03-04 23:12:55 -0800

    [Add] no190 reverse-bits solution code

---
+++ leetcode-java/src/main/java/org/example/no190/ReverseBits.java
@@ -0,0 +1,18 @@
+package org.example.no190;
+
+public class ReverseBits {
+
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            result += n & 1;
+            n >>= 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseBits().reverseBits(4));
+    }
+}
