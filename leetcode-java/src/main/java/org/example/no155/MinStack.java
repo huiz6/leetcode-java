03eb5c1e0a4ddbb7dfe5ab728df59c8baa7ac233
huiz <hzease@gmail.com>
2024-03-07 17:33:59 -0800

    [Add] no155 min-stack solution code

---
+++ leetcode-java/src/main/java/org/example/no155/MinStack.java
@@ -0,0 +1,44 @@
+package org.example.no155;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    private final Stack<Integer> stack, minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (minStack.peek() == val) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+    }
+
+}
